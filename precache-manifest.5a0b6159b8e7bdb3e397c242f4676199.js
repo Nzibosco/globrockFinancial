b59f8e42852a4faeebf4dface50b8026756d45ac
Nzibosco <nzibosco@gmail.com>
2020-09-22 20:35:16 -0400

    Updates

---
+++ precache-manifest.5a0b6159b8e7bdb3e397c242f4676199.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e291765f70de977ccdbd6a2363d3f30b",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "8c2c098a5eae72f8e6cf",
+    "url": "/react-gh-pages/static/css/main.98a321e6.chunk.css"
+  },
+  {
+    "revision": "077fbe511e7a581be97d",
+    "url": "/react-gh-pages/static/js/2.1ddfe1b4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-gh-pages/static/js/2.1ddfe1b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c2c098a5eae72f8e6cf",
+    "url": "/react-gh-pages/static/js/main.3c7aa199.chunk.js"
+  },
+  {
+    "revision": "14b645d2439f68d215a5",
+    "url": "/react-gh-pages/static/js/runtime-main.50e0553d.js"
+  }
+]);
