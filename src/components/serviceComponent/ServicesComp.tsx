97258a22ddc8198e3fdf7a57b7639682ea83e46c
Nzibosco <nzibosco@gmail.com>
2020-10-04 23:18:45 -0400

    image for services in a carousel

--- src/components/serviceComponent/ServicesComp.tsx
+++ src/components/serviceComponent/ServicesComp.tsx
@@ -5,27 +5,35 @@ import React from 'react';
 export default function Services() {
 
     return (
-        <div id="services">
-            <div className="text-center" >
-                <div className="row">
-                    <div className="card service" id="service1">
-                        <h5>Management Consulting</h5>
-                    </div>
-                    <div className="card service" id="service2">
-                        <h5>Investment Advisory</h5>
-                    </div>
-                    <div className="card service" id="service3">
-                        <h5>Financing</h5>
-                    </div>
-                </div>
-            </div>
-            <br />
-            <div className="row text-center">
-                <div className="card service" id="service4">
-                    <h5>Matchmaking</h5>
-                </div>
-            </div>
-        </div>
+<div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active" id="service1">
+      <img src="/Users/bosco/Documents/web and apps/Olivier Kamanzi/globrock_fin/src/service1.png" className="d-block w-100 serviceSlide" alt="invest" />
+            {/* <h3>Investment Advisory</h3> */}
+      
+    </div>
+    <div className="carousel-item" id="service2">
+    <img src="/src/service1.png" className="d-block w-100 serviceSlide" alt="Investment Advisory Service"/>
+      <div className="serviceDescription">
+
+</div>
+    </div>
+    <div className="carousel-item" id="service3">
+    <img src="/src/service1.png" className="d-block w-100 serviceSlide" alt="Investment Advisory Service"/>
+      <div className="serviceDescription">
+
+</div>
+    </div>
+  </div>
+  <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="sr-only">Previous</span>
+  </a>
+  <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="sr-only">Next</span>
+  </a>
+</div>
     )
 
 }
