4cb07b24cd261cfd6da985fa139266da2351ef43
Nzibosco <nzibosco@gmail.com>
2020-11-15 00:45:49 -0500

    search option

--- src/components/navbarComponent/Navbar.tsx
+++ src/components/navbarComponent/Navbar.tsx
@@ -33,7 +33,7 @@ export default function ButtonAppBar() {
           <Typography variant="h6" className={classes.title} id="logo">
             GlobRock Financial
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit">Search</Button>
         </Toolbar>
       </AppBar>
     </div>
