3a3b123b73e853d9665ba74577e5c4e02e844333
Nzibosco <nzibosco@gmail.com>
2020-11-21 21:12:01 -0500

    more footer options

--- src/components/footerComponent/FooterComponent.tsx
+++ src/components/footerComponent/FooterComponent.tsx
@@ -8,19 +8,30 @@ export default function Footer () {
     <footer className="" id="footer">
         <div className = "row">
             <div className="contactUs col-md-4">
-                <h2>Contact us</h2>
-                <h4>Globrock Financial Group, Inc.</h4>
-                <p className = "">Management Consulting | Investment Advisory</p>
-                <p className = "">161 N. Clark Street, Suite 4700</p>
-                <p className = "">Chicago, Illinois 60601 | U.S.A</p>
-                <p className = "">P+1.312.523.2156 | F+1.312.523.2001</p>
-                <p className = "">C+1.312.927.4830</p>
-                <p className = "">e-mail: info@globrockfinancial.com</p>
+                <h6>CONTACT US</h6>
+                <p>Globrock Financial Group, Inc.</p>
+                <small className = "">161 N. Clark Street, Suite 4700</small> <br/>
+                <small className = "">Chicago, Illinois 60601 | U.S.A</small> <br/>
+                <small className = "">P+1.312.523.2156 | F+1.312.523.2001</small> <br/>
+                <small className = "">C+1.312.927.4830</small> <br/>
+                <small className = "">e-mail: info@globrockfinancial.com</small>
             </div>
-            <div className = "servicesFooter">
+            <div className = "servicesFooter col-md-4">
+                <h6>SERVICES</h6>
+                <small className = "">Management Consulting</small> <br/>
+                <small className = "">Investment Advisory</small> <br/>
+                <small className = "">Financial Consulting</small> <br/>
+
+            </div>
+            <div className = "partnersFooter col-md-4">
+                <h6>PARTNERS</h6>
 
             </div>
         </div>
+        <div className = "row" id="social">
+
+        </div>
+        <div className="row" id = "poweredBy"></div>
     </footer>
     );
 }
